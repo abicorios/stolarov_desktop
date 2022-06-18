0d729d83e4e2ae0525272ea714e6b7bb517bd761
abicorios <yaroslav.moroz91@gmail.com>
2022-06-18 08:19:48 +0300

    Set the terminal collors

--- data/data/update.sh
+++ data/data/update.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+if test -f ~/.Xdefaults; then
+    if ! grep -q 'xterm\*background: black' ~/.Xdefaults; then
+        echo 'xterm*background: black' >> ~/.Xdefaults
+    fi
+    if ! grep -q 'xterm\*foreground: lightgray' ~/.Xdefaults; then
+        echo 'xterm*foreground: lightgray' >> ~/.Xdefaults
+    fi
+else
+    echo 'xterm*background: black' >> ~/.Xdefaults
+    echo 'xterm*foreground: lightgray' >> ~/.Xdefaults
+fi
