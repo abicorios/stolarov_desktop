9a0bce67c34fc17c2e7ad3b8a24b6042554c15c3
abicorios <yaroslav.moroz91@gmail.com>
2022-08-28 18:52:18 +0300

    Add bash solution 1

---
+++ data/data/programs/bash/1.2/humpty.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Humpty Dumpty sat on wall,"
+echo "Humpty Dumpty had a great fall."
+echo "All the king's horses and all the king's men"
+echo "Couldn't put Humpty together again."
