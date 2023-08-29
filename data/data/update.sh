74c8c200b6c7ad8c545555303bc3c70972547ede
abicorios <yaroslav.moroz91@gmail.com>
2023-08-29 07:43:56 +0300

    Set the vim line numbers and the bash vi mode

--- data/data/update.sh
+++ data/data/update.sh
@@ -15,6 +15,20 @@ else
     echo 'xterm*foreground: lightgray' >> ~/.Xdefaults
     echo 'XTerm*VT100*selectToClipboard: true' >> ~/.Xdefaults
 fi
+if test -f ~/.vimrc; then
+    if ! grep -q 'set nu' ~/.vimrc; then
+        echo 'set nu' >> ~/.vimrc
+    fi
+else
+    echo 'set nu' >> ~/.vimrc
+fi
+if test -f ~/.bash_login; then
+    if ! grep -q 'set -o vi' ~/.bash_login; then
+        echo 'set -o vi' >> ~/.bash_login
+    fi
+else
+    echo 'set -o vi' >> ~/.bash_login
+fi
 sudo apt update
 sudo apt install dialog apt-utils -y
 sudo apt install -y vim joe nano fpc nasm gcc make gdb mc less
